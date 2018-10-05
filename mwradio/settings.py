3141f81220271e933481b0a8d8bea0f9d8c57f3f
AK <ak@soundelec.co.uk>
2018-10-05 20:35:18 +0100

    miscellaneous additions

--- mwradio/settings.py
+++ mwradio/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0fusxq6w_f!l95h=3(+hw!l0@d6v9_oyes$%uk-tk1_6td%jtl'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-gb'
 
 TIME_ZONE = 'UTC'
 
